49e882fc60ad5766bbb96d4fe24582a59de9ec63
Ivo Jimenez <ivo.jimenez@gmail.com>
2019-02-27 22:20:46 -0700

    Copy libs to correct path

--- docker/imager/entrypoint.sh
+++ docker/imager/entrypoint.sh
@@ -36,7 +36,7 @@ docker run \
   --entrypoint=/bin/bash \
   --volume $INSTALL_DIR:$INSTALL_DIR \
   $CEPH_BASE_DAEMON_IMAGE \
-    -c "cp -r $INSTALL_DIR/bin/* /usr/bin/ && cp -r $INSTALL_DIR/lib/* /usr/lib/"
+    -c "cp -r $INSTALL_DIR/bin/* /usr/bin/ && cp -r $INSTALL_DIR/lib/* /usr/lib64/ceph/"
 
 # commit the above change so that we obtain a new image
 docker commit \
