40361e0c1b200efc0978430a1f4ccb7ba23c4aa4
Ivo Jimenez <ivo.jimenez@gmail.com>
2019-02-28 23:13:26 -0700

    Fixes container startup

--- docker/imager/entrypoint.sh
+++ docker/imager/entrypoint.sh
@@ -52,14 +52,18 @@ docker run \
      make -C $BUILD_DIR install && \
      mkdir -p /opt/ceph-container/bin /etc/ceph && \
      cp -r $BUILD_DIR/daemon/* /opt/ceph-container/bin/ && \
-     echo 'PATH=\$PATH:/opt/ceph-container/bin' > /etc/environment && \
-     echo 'PYTHONPATH=/usr/lib/python2.7/site-packages/' > /etc/environment && \
+     echo '#!/usr/bin/env bash' > new_entrypoint.sh && \
+     echo 'export PATH=\$PATH:/opt/ceph-container/bin' >> new_entrypoint.sh && \
+     echo 'export PYTHONPATH=/usr/lib/python2.7/site-packages/' >> new_entrypoint.sh && \
+     echo 'source /opt/ceph-container/bin/entrypoint.sh' >> new_entrypoint.sh && \
+     chmod +x new_entrypoint.sh && \
+     mv new_entrypoint.sh /usr/bin && \
      ldconfig && \
      useradd -r -s /usr/sbin/nologin ceph"
 
 # commit the above change so that we obtain a new image
 docker commit \
-  --change='ENTRYPOINT ["/opt/ceph-container/bin/entrypoint.sh"]' \
+  --change='ENTRYPOINT ["new_entrypoint.sh"]' \
   cephbase $CEPH_OUTPUT_IMAGE
 
 # cleanup
