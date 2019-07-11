7518a1d14ce0416b68aaeb149b56a80942c23de6
Ivo Jimenez <ivo.jimenez@gmail.com>
2019-07-11 16:55:55 -0700

    rename files

--- workflows/cloudlab/geni/renew.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from geni import util
-
-experiment_name = 'ceph-benchmarks'
-
-ctx = util.loadContext(key_passphrase=os.environ['GENI_KEY_PASSPHRASE'])
-
-util.createSlice(ctx, experiment_name, renew_if_exists=True)
