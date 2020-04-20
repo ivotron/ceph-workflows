e611273963c756d11585c6b7cb35f6152d6958de
Ivo Jimenez <ivo.jimenez@gmail.com>
2020-04-20 09:31:28 -0700

    deploy/cloudlab: extracts cloudlab utility command
    Refactors the geni/config.py script so that the cloudlab utility command
    implemented there is left as a standalone Python module (cloudlab_cmd.py
    file). This allows for other deployment configurations to be reuse this
    logic.

---
+++ deploy/cloudlab/geni/multinode.py
@@ -0,0 +1,30 @@
+from collections import OrderedDict
+
+from .. import cloudlab_cmd
+
+# name of experiment to use to identify this allocation
+experiment_name = 'multi-node-ceph-benchmarks'
+
+# expiration of allocation in minutes
+expiration = 180
+
+# OS image to use
+img = "urn:publicid:IDN+clemson.cloudlab.us+image+schedock-PG0:ubuntu18-docker"
+
+# check availability and types at https://www.cloudlab.us/resinfo.php
+site = 'clemson'
+hw_type = 'c6320'
+
+# whether to create a lan connecting all nodes
+with_lan = True
+
+# grouping of nodes based on their ceph roles (note: insertion order in groups
+# dictionary matters, as that's the order in which nodes are added to request)
+num_osds = 3
+groups = OrderedDict()
+groups['mons'] = ['mon']
+groups['mgrs'] = ['mon']
+groups['osds'] = ['osd{}'.format(n) for n in range(1, num_osds+1)]
+
+cloudlab_cmd.run(experiment_name, expiration, site, groups, hw_type, img,
+                 with_lan)
