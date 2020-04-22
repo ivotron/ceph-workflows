74994fdd34c41c0fbce90c700fb90b2bbe36742c
Ivo Jimenez <ivo.jimenez@gmail.com>
2020-04-22 01:19:11 -0700

    deploy/cloudlab: adds geni-lib single-node configuration

--- deploy/cloudlab/geni/config.py
+++ deploy/cloudlab/geni/config.py
@@ -2,7 +2,7 @@ import cloudlab_cmd
 from collections import OrderedDict
 
 # name of experiment to use to identify this allocation
-experiment_name = 'multi-node-ceph-benchmarks'
+experiment_name = 'popper-ceph'
 
 # expiration of allocation in minutes
 expiration = 180
@@ -14,7 +14,9 @@ img = "urn:publicid:IDN+clemson.cloudlab.us+image+schedock-PG0:ubuntu18-docker"
 site = 'clemson'
 hw_type = 'c6320'
 
-# whether to create a lan connecting all nodes
+############
+# multi-node
+############
 with_lan = True
 
 # grouping of nodes based on their ceph roles (note: insertion order in groups
@@ -25,5 +27,20 @@ groups['mons'] = ['mon']
 groups['mgrs'] = ['mon']
 groups['osds'] = ['osd{}'.format(n) for n in range(1, num_osds+1)]
 
+
+##############
+## single node
+##############
+#with_lan = False
+#groups = {
+#    'mons': ['onlynode'],
+#    'osds': ['onlynode'],
+#    'mgrs': ['onlynode'],
+#}
+
+
+#############
+# run command (parse 'apply', 'destroy', 'get-inventory' and 'renew'
+#############
 cloudlab_cmd.run(experiment_name, expiration, site, groups, hw_type, img,
                  with_lan)
